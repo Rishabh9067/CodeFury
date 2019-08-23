c358815477b603d92734fc47aaa587b6d391163e
Karan Dhama <vkdhama9@gmail.com>
2019-08-23 18:26:45 +0530

    ajax changed...

--- WebContent/Ajax/Ajax_ProductData.js
+++ WebContent/Ajax/Ajax_ProductData.js
@@ -1,5 +1,5 @@
 function searchProducts(){
-  	var type = document.getElementById("ptype").value;
+  	var type = document.getElementById("ptype_drop").value;
   	if(isNaN(type)) {
   		alert("Product Type is not a number");
   	}
@@ -17,23 +17,45 @@ function doAjax(type) {
 		if(ajax.readyState == 4 && ajax.status == 200) {
 			var str = ajax.responseText;
 			var json = JSON.parse(str);
-			
-			//alert(ajax.readyState);
-			document.getElementById("cust_div").style.display = "block";
+			document.getElementById("prod_div").style.display = "block";
 			document.getElementById("err").style.display = "none";
-			document.getElementById("name").innerHTML = json.cname;
-			document.getElementById("address").innerHTML = json.address;
-			document.getElementById("state").innerHTML = json.state;
-			document.getElementById("gst").innerHTML = json.gstNo;
-			document.getElementById("phone").innerHTML = json.phone;
-			document.getElementById("email").innerHTML = json.email;
-			document.getElementById("pincode").innerHTML = json.pincode;
-
+			var ul = document.getElementById("prod_info_ul");
+			ul.innerHTML = '';
+			var rows = Object.keys(json).length;
+			var data="";
+			for(var i=0;i<rows;i++) {
+				var count=0;
+				var li = document.createElement("li");
+				data += "<div id='prod_list'><p><b>Product Name : </b>" + json[i].pname + "</p>";
+				data += "<p><b>Product Type : </b>" + "Level " + json[i].categoryId + "</p>";
+				data += "<input type='hidden' value='"+ json[i].pid +"'>";
+				data += "<p><b>Price : </b>" + json[i].price + "</p>";
+				data += "<button onclick='add_count("+ json[i].pid +")'>+</button> <input type='text' id='count_"+ json[i].pid +"' value='"+ count +"'> <button onclick='subtract_count("+ json[i].pid +")'>-</button></div>"
+				li.innerHTML = data;
+			}
+			ul.appendChild(li);
 		}
 		else {
-			document.getElementById("cust_div").style.display = "none";
+			document.getElementById("prod_div").style.display = "none";
 			document.getElementById("err").style.display = "block";
-			document.getElementById("err").innerHTML = "Customer not found for " + id;
+			document.getElementById("err").innerHTML = "Products not found for level " + type;
 		}
 	};
 }
+
+function add_count(pid) {
+	var count = document.getElementById("count_"+pid).value;
+	if(count==5) {
+		alert("Cannot select any product more than 5 times");
+		return;
+	}
+	document.getElementById("count_"+pid).value = ++count;
+}
+
+function subtract_count(pid) {
+	var count = document.getElementById("count_"+pid).value;
+	if(count==0) {
+		return;
+	}
+	document.getElementById("count_"+pid).value = --count;
+} 
